5b6e230024690523acb8137ea0a522a65940d9eb
Teslimlyon <teslimlyon@gmail.com>
2025-09-14 07:01:31 +0100

    Update Chapter1.hs

--- Practical-Task/Chapter1.hs
+++ Practical-Task/Chapter1.hs
@@ -14,14 +14,24 @@ main :: IO ()
 main = do
  print $ number 3.14 --Answer = 3.14
 
+
+
+word :: [Char] -> [Char]
+word z = "z"
+
+main :: IO () 
+main = do
+ print $ word "z" --Answer = z
  
+           or
+word :: String
 
 add :: Float -> Float -> Float
 add x y = x + y 
 
---main :: IO ()
---main = do
--- print $ add 5.1 2.1 --AAnswer = 7.2
+main :: IO ()
+main = do
+ print $ add 5.1 2.1 --AAnswer = 7.2
 
  
 
