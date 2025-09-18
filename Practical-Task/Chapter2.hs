d352dd83ce5420ed26aa615272e4f17ce5e2a1e2
Teslimlyon <teslimlyon@gmail.com>
2025-09-18 20:02:08 +0100

    Update Chapter2.hs

--- Practical-Task/Chapter2.hs
+++ Practical-Task/Chapter2.hs
@@ -142,5 +142,35 @@ main = do
  print $ isHaskellFun --Output True
  
  
+HC2T4 - Task 4: Converting Between Infix and Prefix Notations
 
+--Use prefix notation for the following infix expressions:
+
+--5 + 3
+--10 * 4
+--True && False
+ 
+ --Solution to the above:
+
+--prefix :: Int -> Int 
+--prefix x = x
+
+--main :: IO ()
+--main = do 
+-- print $ ((+) 5 3) --Output = 8
+
+--prefix :: Int -> Int
+--prefix x = x
+
+--main :: IO ()
+--main = do 
+ --print $ ((*) 10 4) -- Output = 40
+ 
+--expression :: Int -> Bool 
+--expression x = True
+
+--main :: IO ()
+--main = do
+ --print $ ((&&) True False) -- Output= False
+ 
 
