4976edd25716273d266361ea4071c2b13105bd54
Teslimlyon <teslimlyon@gmail.com>
2025-09-18 13:13:50 +0100

    Update Chapter2.hs

--- Practical-Task/Chapter2.hs
+++ Practical-Task/Chapter2.hs
@@ -6,14 +6,14 @@
 "Haskell"
 'Z'
 True && False
-
+     Solution to the above :
+     
 numbee :: Int -> Int 
 number x = 42
 
 main :: IO ()
 main = do
- print $ number 42 --Answer = 42
-
+ print $ number 42 --Output = 42
 
 
 number :: Float -> Float
@@ -21,8 +21,23 @@ number x = 3.14
 
 main :: IO ()
 main = do
- print $ number 3.14 --Answer = 3.14
+ print $ number 3.14 --Output = 3.14
 
+            or
+            
+addNumber :: Float -> Float -> Float
+addNumber x y = x + y 
+
+main :: IO ()
+main = do 
+ print $ addNumber 2.14 1.0
+ --Output = 3.14
+
+
+main :: IO ()
+main = do 
+ print $ "Haskell"
+  --Output = "Haskell"
 
 
 word :: [Char] -> [Char]
@@ -30,7 +45,7 @@ word z = "z"
 
 main :: IO () 
 main = do
- print $ word "z" --Answer = z
+ print $ word "z" --Output = z
  
            or
            
@@ -39,38 +54,56 @@ word z = "z"
 
 main :: IO ()
 main = do
- print $ word "z" --Answer = z
+ print $ word "z" --Output = z
 
-     Function Type Signatures
 
-add :: Float -> Float -> Float
+main :: IO ()
+main = do 
+ print $ True&&True
+ print $ False&&False
+ print $ True&&False
+ print $ False&&True
+  --Output =
+   --True
+   --False
+   --False
+   --False
+
+
+    -- HC2T2 - Task 2: Function Type Signatures
+ A function add that takes two Int values and returns their sum.
+
+add :: Int -> Int -> Int
 add x y = x + y 
 
 main :: IO ()
 main = do
- print $ add 5.1 2.1 --AAnswer = 7.2
+ print $ add 200 20 --Output = 220
 
 
+  A function isEven that takes an Int and returns a Bool indicating if it's even.
 
 isEven :: Int -> Bool
 isEven n = n `mod` 2 == 0
 
 main :: IO ()
 main = do 
- print $ isEven 2 --Answwr True
- print $ isEven 3 -- Answer False
- print $ isEven 100 -- Answer True
+ print $ isEven 2 --Output True
+ print $ isEven 3 -- Output False
+ print $ isEven 100 --Output True
 
-
+ A function concatStrings that takes two String values and returns their concatenation.
 
 concatStrings :: String -> String -> String
 concatStrings x y = x ++ y 
 
 main :: IO ()
 main = do 
- putStrLn (concatStrings "Teslim," "is a good boy")  -- Answer = Teslim is a good boy 
+ putStrLn (concatStrings "Teslim," "is a good boy")  --Output = Teslim is a good boy 
 
- 
-      Immutable Variables
+--HC2T3 - Task 3: Immutable Variables
+ myAge as an Int
+
+
 
 
