77e9355a2705339c3e2becf07e38ece8a19a90aa
Teslimlyon <teslimlyon@gmail.com>
2025-09-21 10:45:35 +0100

    Update Chapter1.hs

--- Practical-Task/Chapter1.hs
+++ Practical-Task/Chapter1.hs
@@ -118,3 +118,17 @@ applyTwice f x = f (f x)
 main :: IO ()
 main = do 
  print (applyTwice (*10) 10)  --Output = 1000   
+
+     or 
+
+applyTwise :: Int -> Int -> Int
+applyTwise a b = 
+ let x = (a + 8) 
+     y = (b + 4) 
+     z = x + y
+  in z
+main :: IO ()
+main = do 
+ print $ applyTwise 6 8
+ print $ applyTwise 8 4
+ print $ applyTwise 6 8
