55605e5b8c8eb9b8cbd5fbc3e63266440311e81f
Teslimlyon <teslimlyon@gmail.com>
2025-09-21 15:08:30 +0100

    Update Chapter3.hs

--- Practical-Task/Chapter3.hs
+++ Practical-Task/Chapter3.hs
@@ -1,1 +1,11 @@
-
+applyTwise :: Int -> Int -> Int
+applyTwise a b = 
+ let x = (a + 8) 
+     y = (b + 4) 
+     z = x + y
+  in z
+main :: IO ()
+main = do 
+ print $ applyTwise 6 8
+ print $ applyTwise 8 4
+ print $ applyTwise 6 8
