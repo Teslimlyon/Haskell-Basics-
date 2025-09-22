3cbe04b87065292ec13dd117ce796fac05790290
Teslimlyon <teslimlyon@gmail.com>
2025-09-22 06:17:57 +0100

    Update Chapter3.hs

--- Practical-Task/Chapter3.hs
+++ Practical-Task/Chapter3.hs
@@ -4,3 +4,21 @@ Define a function checkNumber :: Int -> String.
 Use an if-then-else statement to check if the number is positive, negative, or zero.
 Return "Positive", "Negative", or "Zero" accordingly.
 Test your function with checkNumber 5, checkNumber (-3), and checkNumber 0
+
+
+checkNumber :: Int -> String
+checkNumber n =
+  if n > 0 
+    then "Positive"
+    else if n < 0 
+           then "Negative"
+           else "Zero"
+
+main :: IO ()
+main = do
+  print (checkNumber 5)
+  print (checkNumber (-3))
+  print (checkNumber 0)
+  --Outputs: Positive 
+           --Negative
+           --Zero
