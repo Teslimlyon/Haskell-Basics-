bbf0da7a89f5d7a0270fedc5c7db5b7b41be0b14
Teslimlyon <teslimlyon@gmail.com>
2025-09-22 06:03:29 +0100

    Update Chapter3.hs

--- Practical-Task/Chapter3.hs
+++ Practical-Task/Chapter3.hs
@@ -1,11 +1,6 @@
-applyTwise :: Int -> Int -> Int
-applyTwise a b = 
- let x = (a + 8) 
-     y = (b + 4) 
-     z = x + y
-  in z
-main :: IO ()
-main = do 
- print $ applyTwise 6 8
- print $ applyTwise 8 4
- print $ applyTwise 6 8
+HC3T1 - Task 1: Check if a number is positive, negative, or zero
+
+Define a function checkNumber :: Int -> String.
+Use an if-then-else statement to check if the number is positive, negative, or zero.
+Return "Positive", "Negative", or "Zero" accordingly.
+Test your function with checkNumber 5, checkNumber (-3), and checkNumber 0
