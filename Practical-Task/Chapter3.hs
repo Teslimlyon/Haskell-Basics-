52dc27d99331e1d1743767c49474e0e491d9f5b3
Teslimlyon <teslimlyon@gmail.com>
2025-09-22 06:23:20 +0100

    Update Chapter3.hs

--- Practical-Task/Chapter3.hs
+++ Practical-Task/Chapter3.hs
@@ -22,3 +22,23 @@ main = do
   --Outputs: Positive 
            --Negative
            --Zero
+
+
+
+
+
+
+
+grade :: Int -> String
+grade score
+  | score >= 90 = "A"
+  | score >= 80 = "B"
+  | score >= 70 = "C"
+  | score >= 60 = "D"
+  | otherwise   = "F"
+
+main :: IO ()
+main = do
+  print (grade 95)
+  print (grade 72)
+  print (grade 50)
