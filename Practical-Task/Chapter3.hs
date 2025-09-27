7cf0494c13dd76d1924e6f195acfa0faac01e1b7
Teslimlyon <teslimlyon@gmail.com>
2025-09-27 12:57:03 +0100

    Update Chapter3.hs

--- Practical-Task/Chapter3.hs
+++ Practical-Task/Chapter3.hs
@@ -192,3 +192,22 @@ main = do
   print (season 3)   -- "Spring"
   print (season 7)   -- "Summer"
   print (season 11)  -- "Autumn"
+
+
+
+
+
+bmiCategory :: Float -> Float -> String
+bmiCategory weight height
+  | bmi < 18.5 = "Underweight"
+  | bmi < 25   = "Normal"
+  | bmi < 30   = "Overweight"
+  | otherwise  = "Obese"
+  where
+    bmi = weight / (height ^ 2)
+
+-- Testing
+main :: IO ()
+main = do
+  print $ bmiCategory 70 1.75   -- Expected: "Normal"
+  print $ bmiCategory 90 1.8    -- Expected: "Overweight"
