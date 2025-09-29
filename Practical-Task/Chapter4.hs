1c80e1a4cf4da82985955972fd434ac8ba91c681
Teslimlyon <teslimlyon@gmail.com>
2025-09-29 13:33:18 +0100

    Update Chapter4.hs

--- Practical-Task/Chapter4.hs
+++ Practical-Task/Chapter4.hs
@@ -1,2 +1,18 @@
-
+weatherReport :: String -> String
+weatherReport condition =
+    if condition == "sunny"
+    then "It's a bright and beautiful day!"
+    else if condition == "rainy"
+    then "Don't forget your umbrella!"
+    else if condition == "cloudy"
+    then "A bit gloomy, but no rain yet!"
+    else "Weather unknown"
+    
+main :: IO ()
+main = do
+ putStrLn (weatherReport "rainy")
+ putStrLn (weatherReport "sunny")
+ putStrLn (weatherReport "teslim")
+ putStrLn (weatherReport "cloudy")
+ 
 
