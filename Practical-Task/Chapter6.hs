b09e0aebdf74ba9951a690ec6b98222da2195742
Teslimlyon <teslimlyon@gmail.com>
2025-10-15 09:41:37 +0100

    Update Chapter6.hs

--- Practical-Task/Chapter6.hs
+++ Practical-Task/Chapter6.hs
@@ -68,7 +68,6 @@ main = do
 --HC6T6: Element Exists in List
 Implement a function that determines whether a given element exists in a list.
 
-
 elementExists :: (Eq a) => a -> [a] -> Bool
 elementExists _ [] = False 
 
