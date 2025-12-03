011abdc1c0dd7d8c568335fdf011b637cce953b9
Teslimlyon <teslimlyon@gmail.com>
2025-12-03 10:37:56 -0800

    Update Chapter7.hs

--- Practical-Task/Chapter7.hs
+++ Practical-Task/Chapter7.hs
@@ -66,3 +66,16 @@ main = do
   print (compareValues 10 20)     -- 20
   print (compareValues 'a' 'z')   -- 'z'
   print (compareValues 3.5 2.1)   -- 3.5
+
+
+-- HC7T3: Function Using Multiple Constraints
+
+compareValues :: (Eq a, Ord a) => a -> a -> a
+compareValues x y =
+  if x >= y then x else y
+
+main :: IO ()
+main = do
+  print (compareValues 10 20)     -- 20
+  print (compareValues 'a' 'z')   -- 'z'
+  print (compareValues 3.5 2.1)   -- 3.5
