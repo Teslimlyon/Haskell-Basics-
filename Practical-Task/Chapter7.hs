ccd30e9388945169f6ca4e0da0d9a8aaff34d440
Teslimlyon <teslimlyon@gmail.com>
2025-12-03 10:26:17 -0800

    Update Chapter7.hs

--- Practical-Task/Chapter7.hs
+++ Practical-Task/Chapter7.hs
@@ -1,1 +1,15 @@
-mnn
+-- HC7T1: Implement an Eq Instance for a Custom Data Type
+
+data Color = Red | Green | Blue
+
+instance Eq Color where
+  Red   == Red   = True
+  Green == Green = True
+  Blue  == Blue  = True
+  _     == _     = False
+
+main :: IO ()
+main = do
+  print (Red == Red)
+  print (Green == Blue)
+  print (Blue == Blue)
