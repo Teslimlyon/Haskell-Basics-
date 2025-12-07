86fca5c0e7a1c45712e1349c80efbaf525a47683
Teslimlyon <teslimlyon@gmail.com>
2025-12-06 21:59:00 -0800

    Update Chapter8.hs

--- Practical-Task/Chapter8.hs
+++ Practical-Task/Chapter8.hs
@@ -1,1 +1,13 @@
-b
+type Address = String
+type Value   = Int
+
+generateTx :: Address -> Address -> Value -> String
+generateTx fromAddr toAddr amount =
+    "From: " ++ fromAddr ++ " -> To: " ++ toAddr ++ " | Value: " ++ show amount
+
+main :: IO ()
+main = do
+    let sender = "addr1qsender123"
+    let receiver = "addr1qreceiver456"
+    let value = 100
+    putStrLn (generateTx sender receiver value)
